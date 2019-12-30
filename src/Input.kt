854650fa692044735fb8aae7356af7c2fd917c61
Isaac Miti <ikayz@live.com>
2019-12-30 21:57:26 +0200

    add comments to input

--- src/Input.kt
+++ src/Input.kt
@@ -2,12 +2,16 @@
 Created by ikayz on 30/12/2019
 */
 fun main() {
+    // Ask for name as a string value
     print("Enter name: ")
     var name = readLine().toString()
+    // Ask for age as an integer value
     print("Enter age: ")
     val age = readLine()!!.toInt()
+    // Ask for height as a double value
     print("Enter height: ")
     val height = readLine()!!.toDouble()
+    // Ask for country as a string value
     print("Enter country: ")
     var country = readLine().toString()
 
