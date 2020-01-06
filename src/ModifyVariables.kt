693d45eaee46bb5ad86d30c822f00ef6301abfb8
Isaac Miti <ikayz@live.com>
2020-01-06 11:16:06 +0200

    Add modify variables

---
+++ src/ModifyVariables.kt
@@ -0,0 +1,19 @@
+/*
+Created by ikayz on 06/01/2020
+*/
+fun main() {
+    var counter = 23
+    println("Variable is $counter before modification.")
+
+    counter += 9
+    println("Variable is now: $counter")
+
+    counter -= 9
+    println("Variable is now: $counter")
+
+    counter /= 9
+    println("Variable is now: $counter")
+
+    counter *= 9
+    println("Variable is now: $counter")
+}
