6d7aedd5fb3d0303d7cbdac236d3f4a5951c8fbc
Isaac Miti <ikayz@live.com>
2020-01-06 11:28:28 +0200

    Add age calculating app

---
+++ src/CalculateMyAge.kt
@@ -0,0 +1,17 @@
+import java.util.*
+
+/*
+Created by ikayz on 06/01/2020
+*/
+fun main() {
+    println("== Age Calculator App ==")
+
+    // Get the year from device
+    val currentYearFromDevice = Calendar.getInstance().get(Calendar.YEAR)
+    println("Enter birth year: ")
+    val birthYear: Int = readLine()!!.toInt()
+
+    // Calculate age
+    val age = currentYearFromDevice - birthYear
+    println("Your age is $age years old")
+}
