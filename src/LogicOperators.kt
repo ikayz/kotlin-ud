80e3472abfc5c693e39c6ce255b6e92e824dd654
Isaac Miti <ikayz@live.com>
2020-01-17 09:55:59 +0200

    Add the logic gates

--- src/LogicOperators.kt
+++ src/LogicOperators.kt
@@ -1,3 +1,24 @@
 /*
 Created by ikayz on 17/01/2020
 */
+
+/*
+AND Gate (&&)
+    true && true = true
+    true && false = false
+    false && true = false
+    false && false = false
+
+OR Gate (||)
+    true && true = true
+    true && false = true
+    false && true = true
+    false && false = false
+NOT (!)
+    True = False
+    False = True
+ */
+
+fun main() {
+
+}
