cc7b056c65e32445ddd14349d4cdfbc61524b426
Isaac Miti <ikayz@live.com>
2020-02-20 00:33:25 +0200

    Add simple forloop

---
+++ src/SimpleFor.kt
@@ -0,0 +1,11 @@
+/*
+Created by ikayz on 20/02/2020
+*/
+fun main(args: Array<String>) {
+    println("=== Simple Forloop ===")
+
+    println("== Increment ==")
+    for (i: Int in 1..20 step 2) {
+        println("Number is $i")
+    }
+}
